b8f10b8423987e5dd3a6b8420b8aa3177137028a
eminjafarli <eminjafarli02@gmail.com>
2025-07-19 21:28:08 +0400

    Login now redirects users to the BooksDashboard.js and admins to the HomePage.js

--- bookstore-frontend/src/ProtectedRoute.js
+++ bookstore-frontend/src/ProtectedRoute.js
@@ -1,11 +1,18 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
+
 function ProtectedRoute({ children }) {
     const token = localStorage.getItem("token");
     const role = localStorage.getItem("role");
-    if (!token || role === "USER") {
+
+    if (role === "USER") {
+        return <Navigate to="/books-dashboard" replace />;
+    }
+    else if(!token){
         return <Navigate to="/login" replace />;
     }
+
     return children;
 }
+
 export default ProtectedRoute;
